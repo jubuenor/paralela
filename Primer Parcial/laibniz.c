830dea6f4aeb13cb480d852232e2dafdab3d696c
Jh0mpis <jmorenotr@unal.edu.co>
2023-09-03 12:14:59 -0500

    [add] edit laibniz.c & leibnizFS.c

--- Primer Parcial/laibniz.c
+++ Primer Parcial/laibniz.c
@@ -1,40 +1,40 @@
 #include <stdio.h>
 #include <omp.h>
-#include <time.h>
 
-#define NUM_THREADS 16
+#define NUM_THREADS 1
 #define IT 2e9
 
-clock_t init,end;
-double total_time;
+long double init,end;
+long double total_time;
 
 long double sums[NUM_THREADS];
 long double pi = 0;
-int start[NUM_THREADS];
-int finish[NUM_THREADS];
+long int start[NUM_THREADS];
+long int finish[NUM_THREADS];
 
 void main(){
+
     for(int j=0; j<NUM_THREADS;j++){
         start[j]= (int) (IT/NUM_THREADS)*j;
         finish[j] = (int) (IT/NUM_THREADS)*(j+1)-1;
     }
-    init = clock();
-    #pragma omp parallel num_threads(NUM_THREADS)
-    //printf("%lf \n", ((double)(clock() - init))/CLOCKS_PER_SEC);
+
+    omp_set_num_threads(NUM_THREADS);
+    init = omp_get_wtime();
+    #pragma omp parallel
     {
         int ID = omp_get_thread_num();
 
-        for(int i = start[ID]; i<=finish[ID]; i++){
+        for(long int i = start[ID]; i<=finish[ID]; i++){
             sums[ID] += (i&1)? ((long double) -4/(long double) (2*i+1)): ((long double) 4/(long double) (2*i+1));
         }
     }
-    end = clock();
-    total_time = ((double)(end - init))/CLOCKS_PER_SEC;
+    end = omp_get_wtime();
+    total_time = end - init;
 
     for(int j=0; j<NUM_THREADS;j++){
         pi+=sums[j];
-        //printf("%d  %d\n", start[j], finish[j]);
     }
     printf("%.15Lf\n", pi);
-    printf("%d , %lf \n", NUM_THREADS,total_time);
+    printf("%d , %Lf \n", NUM_THREADS,total_time);
 }
--- Primer Parcial/leibnizFS.c
+++ Primer Parcial/leibnizFS.c
@@ -2,13 +2,13 @@
 #include <omp.h>
 #include <time.h>
 
-#define NUM_THREADS 16
+#define NUM_THREADS 1
 #define IT 2e9
 
-clock_t init,end;
-double total_time;
+long double init,end;
+long double total_time;
 
-long double sums[NUM_THREADS];
+long double sums[NUM_THREADS][64];
 long double pi = 0;
 long int start[NUM_THREADS];
 long int finish[NUM_THREADS];
@@ -18,23 +18,21 @@ void main(){
         start[j]= (int) (IT/NUM_THREADS)*j;
         finish[j] = (int) (IT/NUM_THREADS)*(j+1)-1;
     }
-    init = clock();
+    init = omp_get_wtime();
     #pragma omp parallel num_threads(NUM_THREADS)
-    printf(".");
     {
         int ID = omp_get_thread_num();
 
         for(long int i = start[ID]; i<=finish[ID]; i++){
-            sums[ID] += (i&1)? ((long double) -4/(long double) (2*i+1)): ((long double) 4/(long double) (2*i+1));
+            sums[ID][0] += (i&1)? ((long double) -4/(long double) (2*i+1)): ((long double) 4/(long double) (2*i+1));
         }
     }
-    end = clock();
-    total_time = ((double)(end - init))/CLOCKS_PER_SEC;
+    end = omp_get_wtime();
+    total_time = end - init;
 
     for(int j=0; j<NUM_THREADS;j++){
-        pi+=sums[j];
-        //printf("%d  %d\n", start[j], finish[j]);
+        pi+=sums[j][0];
     }
     printf("%.15Lf\n", pi);
-    printf("%d , %lf \n", NUM_THREADS,total_time);
+    printf("%d , %Lf \n", NUM_THREADS,total_time);
 }
